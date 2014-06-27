ee87c5c30e151419e0336cce9f5a2f91a5f28489
devonbeard <devonbeard88@gmail.com>
2014-06-26 20:42:45 -0700

    Setting up js object

--- fewd/unit-3/quiz-app/js/app.js
+++ fewd/unit-3/quiz-app/js/app.js
@@ -3,25 +3,40 @@
 
 // 2) Users should be able to to input their answer to each question, with radio buttons or some other appropriate interface.
 
-
 // 3) When the user answers the last question, the app should show his or her overall score.
 
-
-// 4) Each question should be stored as a JavaScript object, and you’ll probably want to store your list of questions in an array.
-
 // Additional:
-
 // 1) Let the user know where in the quiz they are at each step (i.e. “Question 3 of 5")
-
 // 2) Let the user know their score so far
-
 // 3) Let the user know if their previous response was correct
 
-var questions = object();
-var question1 = new;
+var quizApp = {
+
+  questions: [
+    'What does CSS stand for?',
+    'What does HTML stand for?',
+    'What does CSS stand for?',
+    'What does CSS stand for?'
+  ],
+
+  questionAnswers: [
+    'Cascading Style Sheets',
+    'Hypertext Markup Language',
+    'Cascading Style Sheets',
+    'Cascading Style Sheets'
+  ],
+
+  scoreCalculator: function() {
+    return 'Hello world';
+  }
+
+};
+
+console.log(quizApp.questions[0]);
+console.log(quizApp.scoreCalculator());
 
 
-$(document).ready(function(){
+// $(document).ready(function(){
 
 
-});
+// });
